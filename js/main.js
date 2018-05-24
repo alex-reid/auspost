dcd2072e8f5d9c17adae438b51e9969730e87806
Unknown <alex@alex-reid.com.au>
2018-05-24 17:34:01 +1000

    colours, icons and screensaver

--- js/main.js
+++ js/main.js
@@ -7,6 +7,84 @@ var menu = new Vue({
   methods: {
   	toggleMenu: function(){
   		this.isOpen = !this.isOpen;
+  	},
+  	load_ff: function(){
+  		this.isOpen = false;
+  		this.title = "Fast Facts"
+  	},
+  	load_pp: function(){
+  		this.isOpen = false;
+  		this.title = "Pain Points"
+  	},
+  	load_cs: function(){
+  		this.isOpen = false;
+  		this.title = "Case Studies"
   	}
   }
 })
+
+
+var ss_slide;
+var screensaver = new Vue({
+	el: '#screensaver',
+	data: {
+		open: true,
+		text: [
+			{id: 1, text:"Our solutions"},
+			{id: 2, text:"solve"},
+			{id: 3, text:"your biggest"},
+			{id: 4, text:"pain points"},
+			{id: 5, text:"in disbursement"},
+			{id: 6, text:"and collection"}
+		],
+		current: 0
+	},
+	methods: {
+		init: function(){
+			this.destroy;
+			this.open = true;
+			ss_slide = setInterval(this.textFade, 2000);
+			menu.title = "";
+		},
+		textFade: function(){
+			var items = this.text.length;
+			if(this.current++ >= items) this.current = 1;
+		},
+		destroy : function(){
+			clearInterval(ss_slide);
+			this.current = 1;
+			this.open = false;
+		}
+	}
+})
+
+var fast_facts = new Vue({
+	el: '#fast_facts',
+	data: {
+		open: false,
+	}
+})
+
+
+// jquery
+
+var ss_timeout;
+var time = 5000;
+
+function timeout(){
+	ss_timeout = setTimeout(function(){
+		if(!screensaver.open) screensaver.init();
+	},time)
+}
+
+$(document).ready(function() {
+
+	// init timeout
+	timeout();
+	$(window).click(function(event) {
+		screensaver.destroy();
+		clearTimeout(ss_timeout);
+		timeout();
+	});
+	screensaver.init();
+});
